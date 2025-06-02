bf0d2a7d3d5e56662e10e550ac95b0a68e11291e
DuytungTT <duytung2506@gmail.com>
2025-06-02 17:21:32 +0700

    update 2/6/25

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀 Server chạy tại http://localhost:${PORT}`);
 });
